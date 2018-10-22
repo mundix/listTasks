2c84f5e4fed561641fd3976a9d5f2fa954189931
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-22 11:43:18 -0400

    Creando el input de los tasks

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" class="container">
    <div class="jumbotron">
      <titulo v-bind:titulo="titulo"></titulo>
+     <nueva-tarea v-bind:tasks="tasks"></nueva-tarea>
    </div>
   </div>
 </template>
@@ -9,13 +10,34 @@
 <script>
 
 import Titulo from './TituloCompoment';
+import NuevaTarea from './NewTaskComponent'; // Anñadirlo a la lista de componente
 export default {
   components: {
-    Titulo
+    Titulo,
+    NuevaTarea //Ahora voy a pasarlo al componente, si es CammelCase <cammel-case></cammel-case> en este caso es
+  //<nueva-tarea></nueva-tarea>
   },
   data() {
     return {
-      titulo : "Task List"
+      titulo : "Task List",
+      tasks : [
+        {
+          text: 'Learn Vue.js',
+          finished: false
+        },
+        {
+          text: 'Learn Angular ',
+          finished: false
+        },
+        {
+          text: 'Learn React',
+          finished: false
+        },
+        {
+          text: 'Learn Ionic 2',
+          finished: false
+        },
+      ]
     }
   }
 }
---
+++ src/NewTaskComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="input-group">
+    <input type="text" placeholder="Write a new Task" v-model="newTask"
+      class="form-control" v-on:keyup:keyup.enter="addNewTask"/>
+    <span class="input-group-btn">
+      <button type="button"
+      v-on:click="agregarTarea"
+      class="btn btn-primary"> Add </button>
+    </span>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        newTask: ''
+      }
+    },
+    props: ['tasks'], //asi se recibe la variable enviada en v-on:tasks
+    methods: {
+      addNewTask() {
+        var text = this.newTask.trim();
+        if(text) {
+          this.tasks.push({
+            text: text,
+            finished:false
+          }),
+          this.newTask = '';
+        }
+      }
+    }
+  }
+</script>
