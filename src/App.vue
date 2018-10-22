fa3f8c567f6dfb3f3771a4a34ebabaa22626fd53
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-22 08:09:54 -0400

    start Repo

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="container">
+   <div class="jumbotron">
+     <Titulo></Titulo>
+   </div>
+  </div>
+</template>
+
+<script>
+
+import Titulo from './TituloCompoment';
+export default {
+  components: {
+    Titulo
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
